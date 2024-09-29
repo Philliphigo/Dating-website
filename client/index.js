ea9a428f1f5e2a474e129c7af2c0ea4e2dea9a2b
Philliphigo <144315556+Philliphigo@users.noreply.github.com>
2024-09-29 22:42:52 +0200

    Create index.js

---
+++ client/index.js
@@ -0,0 +1,9 @@
+// index.js
+
+const root = document.getElementById('root');
+
+root.innerHTML = `
+  <h1>Welcome to the Dating Website</h1>
+  <p>Find your match and connect with people.</p>
+  <a href="/login">Login</a> | <a href="/signup">Sign Up</a>
+`;
