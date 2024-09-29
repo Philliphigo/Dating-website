84e243b8c07dbddff7b49f79d2f81da7f2ab0c9d
Philliphigo <144315556+Philliphigo@users.noreply.github.com>
2024-09-29 23:20:59 +0200

    Create Home.js

---
+++ client/Home.js
@@ -0,0 +1,14 @@
+// client/Home.js
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Find your match and connect with people.</p>
+      <a href="/login">Login</a> | <a href="/signup">Sign Up</a>
+    </div>
+  );
+}
+
+export default Home;
