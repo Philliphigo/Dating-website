2b339557dcec55ad669569b0dc378dfb03060d9b
Philliphigo <144315556+Philliphigo@users.noreply.github.com>
2024-10-05 18:37:00 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Make sure this path is correct
+
+ReactDOM.render(<App />, document.getElementById('root'));
